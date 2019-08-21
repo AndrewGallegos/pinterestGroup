70386c59bd2ee74ac3d6f3a6d2c3d27b18dcb58e
AndrewGallegos <andrew.gallegos.professional@gmail.com>
2019-08-21 15:45:31 -0600

    merged code

--- pageObjects/pinterestPage.js
+++ pageObjects/pinterestPage.js
@@ -16,6 +16,50 @@ module.exports = {
         passInput: 'input[type=password]',
         //pinterest homepage
         mainPage:'.appContent',
-        header: 'h5'
+        header: 'h5',
+        searchBtn: {
+            selector: '//input[@type="text"]',
+            locateStrategy: 'xpath'
+        },
+        selectPicture: {
+            selector: '(//div[@class="MIw QLY Rym ojN p6V zI7 iyn Hsu"])[6]',
+            locateStrategy: 'xpath'
+        },
+        saveButton: {
+            selector: '(//div[@class="Jea gjz zI7 iyn Hsu"])[3]',
+            locateStrategy: 'xpath'
+        },
+        createBoard: {
+            selector: '//*[@data-test-id="createBoardButton"]',
+            locateStrategy: 'xpath'
+        },
+        boardName: {
+            selector: '//input[@class="wyq iyn Hsu aZc tBJ dyH iFc SMy yTZ L4E edc pBj qJc fgH"]',
+            locateStrategy: 'xpath'
+        },
+        submitBoardBtn: {
+            selector: '//button[@class="RCK Hsu mix Vxj aZc GmH adn Il7 Jrn hNT iyn BG7 NTm KhY"]',
+            locateStrategy: 'xpath'
+        },
+        controlPannel: {
+                selector: '//div[@data-test-id= "button-container"]',
+                locateStrategy: 'xpath'
+        },
+        profilePannel: {
+            //selector: '(//*[@data-test-id="button-container"])/*[3]',
+            selector: '(//div[@class="Jea gjz hA- hs0 mQ8 wYR zI7 iyn Hsu"])[3]',
+            locateStrategy: 'xpath'
+        },
+        boardButton: {
+            selector: '//*[@role="tab"][1]',
+            locateStrategy: 'xpath'
+        },
+        newBoardSelect: {
+            selector: '(//div[@class= "Mhr Zr3 sLG zI7 iyn Hsu"])[1]',
+            locateStrategy: 'xpath'
+        },
+        boardEditButton: 'button[aria-label="Edit board"]',
+        boardDeleteButton: '[data-test-id="deleteBoardButton"]',
+        boardConfirmDelete: '[data-test-id="confirmBoardDelete"]'
     }
 }
--- tests/pinterestTest.js
+++ tests/pinterestTest.js
@@ -1,41 +1,80 @@
-var pinFill = function (pinObjects, pinInfo) {
+var pinFill = function (pinObjects, pinData) {
     pinObjects
-    .waitForElementVisible('@controlPannel')
-    .click('@searchBtn')
-    .setValue('@searchBtn', [pinInfo.sea, browser.Keys.ENTER])
-    .click('@selectPicture')
-    .click('@saveButton')
-    .click('@createBoard')
-    .setValue('@boardName', pinInfo.boa)
-    .verify.valueContains('@boardName', "Pins on Pins")
-    .click('@submitBoardBtn')
-    .waitForElementVisible('@controlPannel')
-    .click('@profilePannel')
-    .waitForElementVisible('@boardButton')
-    .click('@boardButton')
-    .waitForElementVisible('@newBoardSelect')
-    .verify.valueContains('@newBoardSelect')
-    .click('@newBoardSelect')
-    .verify.valueContains('@selectPicture')
- }
- var pinData =[
-    {
-        sea: 'Enamel Pins',
-        boa: 'Pins on Pins'
+        .waitForElementVisible('@controlPannel')
+        .click('@searchBtn')
+        .setValue('@searchBtn', [pinData.sea, pinObjects.api.Keys.ENTER])
+        .click('@selectPicture')
+        .click('@saveButton')
+        .click('@createBoard')
+        .setValue('@boardName', pinData.boa)
+        .verify.valueContains('@boardName', pinData.boa)
+        .click('@submitBoardBtn')
+        .waitForElementVisible('@controlPannel')
+        .pause(7000)
+        .click('@profilePannel')
+        .waitForElementVisible('@boardButton')
+        .click('@boardButton')
+        .waitForElementVisible('@newBoardSelect')
+        .expect.element('@newBoardSelect').text.to.contain(pinData.boa)
+        //teardown
+    page.click('@newBoardSelect')
+        .pause(500)
+        .waitForElementVisible('@boardEditButton')
+        .click('@boardEditButton')
+        .pause(500)
+        .click('@boardDeleteButton')
+        .pause(500)
+        .click('@boardConfirmDelete')
+}
+var pinData = {
+    sea: 'Enamel Pins',
+    boa: String(Math.random()*Math.ceil(99999999)) //random board name
+}
+var page;
+module.exports = {
+    before: browser => {
+        page = browser.page.pinterestPage()
+        page.resizeWindow(1366,768)
+            .navigate()
     },
- ]
- var pinObjects = {}
- module.exports = {
-    beforeEach: browser =>{
-        pinObjects = browser.page.pinterest()
-        //pinObjects.navigate()
+    after: function () {
+        page.end()
     },
-    after: browser => {
-        pinObjects.end()
-    },
-    'Create Board Test': browser => {
-        pinData.forEach(test => {
-            pinFill(pinObjects, test)
+    'Login': browser => {
+        var credentials = {
+            name: 'Barbara',
+            email: 'pinterest.test.email@gmail.com',
+            password: 'timtp123'
+        }
+        //click 'Login with Google' button and switch to the new window
+        page.waitForElementVisible('@googleBtn')
+            .click('@googleBtn')
+            .pause(500)
+            .api.windowHandles(function (result) {
+                page.switchWindow(result.value[1])
+            })
+        //wait for new email input to be visible, then input email and press enter
+        page.waitForElementVisible('@emailInput')
+            .clearSet('@emailInput', credentials.email)
+            .api.keys(browser.Keys.ENTER)
+        //wait for page switch animation to play, then input password and press enter
+        page.pause(1000)
+            .clearSet('@passInput', credentials.password)
+            .api.keys(browser.Keys.ENTER)
+        page.api.windowHandles(function (result) {
+            page.switchWindow(result.value[0])
         })
-    }
- }
+        //after switching back to original window, wait for main page to load and then run assertion
+        page.waitForElementVisible('@mainPage')
+            .expect.element('@profilePannel').text.to.contain('Barbara')
+    },
+    'Create Board Test': function() {
+        // pinData.forEach(test => {
+        //     pinFill(pinObjects, test)
+        // })
+        pinFill(page, pinData)
+    },
+    // 'Delete Board Test': function() {
+
+    // }
+}
